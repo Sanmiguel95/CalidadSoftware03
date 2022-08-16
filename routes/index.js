5375ddaba292cf625a94c9030e276994f3346c1a
Camilo Sanmiguel <camilosanmiguel81@gmail.com>
2022-08-16 14:37:55 -0500

    Hola Mundo en Heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Express');
+  res.send('Hola Mundo :D');
   // res.render('index', { title: 'Express' });
 });
 
