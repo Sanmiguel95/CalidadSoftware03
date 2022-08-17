cfcb818061bb5c6002f461f7344831eb00c12320
Camilo Sanmiguel <camilosanmiguel81@gmail.com>
2022-08-16 20:11:19 -0500

    socializacion 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hola Mundo :D, ¿Como estan?');
+  res.send('Express');
   // res.render('index', { title: 'Express' });
 });
 
